a23ec2b13a898390b3e59f8cbf283b3eea6eade7
AtchyuteswarGottumukkala <yt.atchyut@gmail.com>
2024-07-22 17:58:22 +0530

    display

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyB2AhBMMfP9Xlg16EMGaeNUcrbhnNNVh5U",
   authDomain: "reactchat-8eda5.firebaseapp.com",
   projectId: "reactchat-8eda5",
   storageBucket: "reactchat-8eda5.appspot.com",
